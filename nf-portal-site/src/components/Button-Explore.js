a3349b762c8e66129befb89fbab85fbb803e7a76
mikeybkats <michaelbkats@gmail.com>
2018-10-26 14:28:15 -0700

    styling updates to navigation

--- nf-portal-site/src/components/Button-Explore.js
+++ nf-portal-site/src/components/Button-Explore.js
@@ -11,7 +11,7 @@ class Button extends Component {
 
   render() {
     return (
-      <div className="row flex justify-end">
+      <div className="row flex justify-end explore-button">
         <button type="button" className="btn-explore" onClick={() => this.changeRoute(this.props.url)}>
           <h5>
             Explore
