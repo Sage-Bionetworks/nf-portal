0e0263e531fed3097ba4d1e447dfbb26d760de8b
Michael Lee <michael.leem42@gmail.com>
2019-04-03 09:11:00 -0700

    reduced size of header box

--- nf-portal-site/src/components/Home-hero.js
+++ nf-portal-site/src/components/Home-hero.js
@@ -2,18 +2,14 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <section className="row hero">
-      <div className="container flex">
-        <div className="row hero-message">
-          <div className="col-sm-9">
-            <h1>A home for Neurofibromatosis research resources</h1>
-            <p>
-              The NF Data Portal is a conduit for ongoing and published research activity related to Neurofibromatosis and Schwannomatosis. It contains data, analysis tools, and publications with the goal of accelerating the development of new treatments for NF.
-              {" "}
-              <a href="https://staging.nf.synapse.org/#/About">Learn more here.</a>
-            </p>
-          </div>
-        </div>
+    <section className="hero flex">
+      <div className="hero-message">
+        <h1>A home for Neurofibromatosis research resources</h1>
+        <p>
+          The NF Data Portal is a conduit for ongoing and published research activity related to Neurofibromatosis and Schwannomatosis. It contains data, analysis tools, and publications with the goal of accelerating the development of new treatments for NF.
+          {" "}
+          <a href="https://staging.nf.synapse.org/#/About">Learn more here.</a>
+        </p>
       </div>
       <div className="hero-background" />
     </section>
